c317cd9db47e921b7fa1aa252d13170b35969f1b
Willy I. Komara <32428649+codingwill@users.noreply.github.com>
2022-10-15 02:46:03 +0700

    Time: 26 ms (21.95%), Space: 8.3 MB (68.01%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.cpp
+++ 0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -3,24 +3,21 @@ public:
     string licenseKeyFormatting(string s, int k) {
         int n = s.size();
         int countIndex = 0;
-        vector<char> ansArr;
         string ans = "";
         for (int i = n-1; i >= 0; --i) {
             if (s[i] != '-') {
                 if (countIndex == k) {
-                    ansArr.push_back('-');
+                    ans.push_back('-');
                     countIndex %= k;
                 }
                 countIndex++;
                 if (s[i] >= 'a') {
                     s[i] = s[i] - ('a' - 'A');
                 }
-                ansArr.push_back(s[i]);
+                ans.push_back(s[i]);
             }
         }
-        for (int i = ansArr.size()-1; i >= 0; --i) {
-            ans.push_back(ansArr[i]);
-        }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
